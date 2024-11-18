c2ddede2f03993e6be1e415c43257c445861018d
C9b3rD3vi1 <nicksonwe@gmail.com>
2024-11-18 18:50:41 +0300

    interactive

--- main.go
+++ main.go
@@ -1,22 +1,27 @@
 package main
+
 import "fmt"
 
-
 func main() {
+	// Uncomment the desired mode
 
-    // Brute force attack
-	fmt.Println("Entering password guessing mode...")
+	// Mode 1: Run the brute force simulation
+ fmt.Println("Entering password guessing mode...")
 	bruteForceAttack()
-	
+
+	// Mode 2: Interactive login system
+	// Uncomment this block to test interactive login
+	/*
+	fmt.Println("Entering interactive login mode...")
 	for {
 		var username, password string
 		fmt.Print("Enter username: ")
 		fmt.Scan(&username)
 		fmt.Print("Enter password: ")
 		fmt.Scan(&password)
-
-		if login(username, password) {
+		if Login(username, password) {
 			break
 		}
 	}
+	*/
 }
